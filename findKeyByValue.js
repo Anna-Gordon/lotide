2bed608eb7ba636b366bf3c5edc9e07415a72f22
Anna Gordon <annetta@email.com>
2020-02-05 23:21:03 +0000

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const findKeyByValue = function(shows, name) {
+
+  for (let show in shows) {
+    if (shows[show] === name) {
+      return show;
+    }
+  }
+ 
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍓🍓🍓 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
