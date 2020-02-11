3626f9f68522328b4ca9657b6168f455521675b8
Anna Gordon <annetta@email.com>
2020-02-11 00:46:48 +0000

    Mocha test for tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,24 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("#middle", () => {
+
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should return [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("should return [1, 2, 3, 4, 5] for [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
